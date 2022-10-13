4e94ff7d765a428df766238e82d58a55c8936773
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-13 05:30:34 -0700

    Extra

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Prints to standard error
+ *
+ * Return: returns 1 (error)
+ *
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of ar is useful\" - Dora Korpa, 2015-10-19");
+
+	return (1);
+
+}
