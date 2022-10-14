8bd9eedec5e989bfcee34a9263f2910d9249cba9
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-14 14:11:27 +0100

    A code that prints out numbers 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints out number 0-9
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+
+	puts("\n");
+
+	return (0);
+
+}
