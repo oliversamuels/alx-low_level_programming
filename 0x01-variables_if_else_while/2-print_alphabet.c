a140065c76e0ff060b452551d9cbaa19f3472923
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-14 13:40:15 +0100

    A code that prints the alphabets a - z in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets a-z in lowercase
+ *
+ * Return: returns 0 after a successful op
+ *
+ */
+
+int main (void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
