cd3dce85b433f4e950f8016bbc3bfab84e18882c
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-18 09:58:09 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add - Adds two int and return the result
+  * @x, @y: Integers to be added
+  * Return: the summed value
+  */
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	return (result);
+}
