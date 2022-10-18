d92e4f4f6eb9811202c019ee63561886ec79a12f
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-18 08:48:39 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_sign - Prints the signs
+  *
+  * Return: Always 0 (success)
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	_putchar('0');
+	return (0);
+}
