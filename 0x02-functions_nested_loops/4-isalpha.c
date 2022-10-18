e0c5e0f3a200417e1adfd47977aea8c0fa3832ca
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-18 14:06:24 +0100

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - checks if a parameter is alphabet
+ * @c: Parameter to be checked
+ * Return: Returns 1 if it is alphabet and 0 if its not
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
