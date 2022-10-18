c545d1429d9dbb899d70649e17122669d4fc6109
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-18 09:07:29 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * print_last_digit - Prints the last digit
+  * @n: The parameter to be checked
+  * Return: Always 0 (success)
+  */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
