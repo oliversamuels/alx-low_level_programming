2162990ac85add6096a9e8b41db2216601649d26
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-18 09:47:41 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+  * times_table - Prints 9 Times table start with 0
+  *
+  * Return: Always 0 (success)
+  */
+
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		
+		for (y = 1; y <= 9; y++)
+		{
+			z = (x * y);
+			if ((z / 10) > 0)
+			{
+				_putchar((z / 10) + '0');
+			} else 
+			{
+				_putchar(' ');
+			}
+
+			_putchar((z % 10) + '0');
+
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
