886314fe4c2510e016a57627838b95562eab564e
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-20 02:41:55 -0700

    More Function - Task 2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
-/** _isdigit - check is a parameter is digit
+/**
+  * _isdigit - check is a parameter is digit
   * @c: The parameter to be checked
   * Return: 1 if @c is digit, 0 otherwise
   */
 
 int _isdigit(int c)
 {
-	if (c >= '0' && c<= '9')
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * mul - Multiplies two integer
+  * @a: First integer
+  * @b: Second integer
+  * Return: The multiplication of @a and @b
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
