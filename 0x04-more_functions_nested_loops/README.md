c2f8929d583586aa45a5280dd470b4f59574ceeb
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-20 02:14:56 -0700

    More Functions - Task 0

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+Introduction to C Programming
+- Functions and Loops (More)
+
+This dir contains practice codes on functions
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+void testFunction(void);
+int _isupper(int c);
+
+#endif
