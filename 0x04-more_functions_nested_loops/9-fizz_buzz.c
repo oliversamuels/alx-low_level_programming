4ef8e8ce6af8255478d11df7cee54ee35caec814
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-20 05:45:46 -0700

    More Function - Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - Print numbers
+  * Return: 0
+  */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((x % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((x % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
