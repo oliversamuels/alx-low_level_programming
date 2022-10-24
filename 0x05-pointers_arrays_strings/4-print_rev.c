2c861b5226ce298b87dcf8d0c9681ac49ff1ebc3
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-24 04:35:01 -0700

    Pointers and Array - Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * print_rev - Prints the reverse of a string.
+  * @s: The string to be reversed
+  * Return: Always 0
+  */
+
+void print_rev(char *s)
+{
+	int i, x;
+
+	x = strlen(s);
+
+	for (i = x; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
