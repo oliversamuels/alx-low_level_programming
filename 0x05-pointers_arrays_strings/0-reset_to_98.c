a5de3312ab8f734d2e997d6988e3ced7e3890340
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-24 02:32:27 -0700

    Pointers and Array - Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - Resets a given int to '98'
+  * @*n: The pointer to the value to be reset.
+  * Return: Always 0 (success)
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+Introduction to C - Programming
+* Pointers and Array
+
+Dir contains work practice on the use of pointers in C programming.
