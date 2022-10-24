12d26dce5452d58b7591c13c041fe65a851b1097
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-24 03:14:14 -0700

    Pointers and Arrays - Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strlen - Returns the lenght of a string
+  * @s: The string to work on
+  * Return: void
+  */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; count[s] != '\0'; count++);
+	
+	return (count);
+}
