0574283740edcbc49c1366fd328569afe44256ec
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-10-24 07:42:04 -0700

    Pointer and Arrays - Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * puts2 - Prints every character of a string
+  * @str: String to be printed
+  * Return: Always 0
+  */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		if ((j % 2) == 0)
+		{
+			_putchar(str[j]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+
+	_putchar('\n');
+}
