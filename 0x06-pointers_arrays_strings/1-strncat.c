39d35fbc5ce21022ec7111f98c050dbcf39c928c
Oliversamuels <amoskevinsamuel@yahoo.com>
2022-10-28 13:04:41 +0100

    Pointer and Strings - Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncat - A function that concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ * @n: Number of byte to use from source string
+ * Return: Destination string is returned
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		dest[i + j] = src[j];
+		if (src[j] == '\0')
+		{
+			j = n;
+		}
+	}
+
+	return (dest);
+
+}
