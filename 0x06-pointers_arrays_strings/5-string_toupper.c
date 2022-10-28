c19166a12ca990a8efc027a6ecdca7fd5275697a
Oliversamuels <amoskevinsamuel@yahoo.com>
2022-10-28 13:56:57 +0100

    Pointers and String - Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper - Converts a string to upper
+ * @n: All strings
+ * Return: capitalized string
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+	}
+	return (n);
+}
