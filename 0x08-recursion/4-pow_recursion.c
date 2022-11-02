635ec19d5ada2ed3a6e68b298f5c21d7b78b1037
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-11-02 04:31:07 -0700

    Recursion - Task4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _pow_recursion - Entry Point
+  * @x: Input
+  * @y: Input
+  * Return: Always 0
+  */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
