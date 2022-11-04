0d03109f79b3bb21380716f568dc8c69529a54d3
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-11-04 08:12:36 -0700

    Arguments - Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the name of the file
+  * @argc: input
+  * @argv: input
+  * Return: Always 0
+  */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+Introduction to low level Programming - C
+* Argument Count and Argument Vector
