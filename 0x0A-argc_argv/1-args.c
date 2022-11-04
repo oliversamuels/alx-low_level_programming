ef0ab601c7526c7a906d7de01a3d674a4c03cbbb
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-11-04 08:31:25 -0700

    Arguments - Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the number of argument
+  * @argc: input
+  * @argv: input
+  * Return: Always 0
+  */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
