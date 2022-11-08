f705fb7ec175ffd94d9954f72eb6fc894fba9557
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-11-08 14:25:08 -0800

    Malloc, Free - Task 0 - 4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		iarray = malloc(sizeof(int) * width);
+		iarray[i] = malloc(sizeof(int) * width);
 		if (iarray == NULL)
 		{
 			free(iarray);
