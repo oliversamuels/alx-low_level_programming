3daf1bd0385ae7a70ceafa5f3a8dd1b04aa16c17
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-11-16 04:13:36 -0800

    Function Pointer - Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,13 +15,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	unsigned int i;
 
 	if (array == NULL || cmp == NULL)
-		return;
+		return (-1);
 	if (size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		if (cmp(array[i]) != 0)
 		{
 			return (i);
 		}
