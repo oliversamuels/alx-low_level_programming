f6f88df1b3e43ba50d754f6def70ef83c2ebdddf
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-11-16 03:54:20 -0800

    Function Pointer - Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  * array_iterator - Entry Point
+  * @array: Input
+  * @size: Input
+  * @action: Input Function
+  * Return: Always 0
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
