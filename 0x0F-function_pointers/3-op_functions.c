8329e9d1d7ec09b47a2f91f1b87a334494bcc565
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-11-17 09:25:49 -0800

    Funtion Pointer - Task 4

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+  * op_add - Entry Point
+  * @a: Input
+  * @b: Input
+  * Return: Always 0
+  */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - Entry Point
+  * @a: Input
+  * @b: Input
+  * Return: Always 0
+  */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - Entry Point
+  * @a: Input
+  * @b: Input
+  * Return: Always
+  */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - Entry Point
+  * @a: Input
+  * @b: Input
+  * Return: Always 0
+  */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - Entry Point
+  * @a: Input
+  * @b: Input
+  * Return: Always 0
+  */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
