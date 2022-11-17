937975ed5b514d91946fef49aa0a59d2347a65c0
Samuel Amos <amoskevinsamuel@hotmail.com>
2022-11-17 10:24:24 -0800

    Variadic Function - Task 3

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,5 @@
-#include <stdio.o>
-#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
 
